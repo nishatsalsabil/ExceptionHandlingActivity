3482b7a0a9eec183521f04237d4973ebd3da5e88
Nishat <nsalsabil25@gmail.com>
2022-03-30 00:38:19 -0700

    handling exception errors

---
+++ activity/main.py
@@ -0,0 +1,11 @@
+def having_an_exception(country):
+    if country == "Texas":
+        raise ValueError 
+    return True 
+
+
+
+# Ex.2
+# def find_shrek_2(movies):
+#     movie = movies["Shrek 2"]
+#     return movie 
--- tests/test_exception.py
+++ tests/test_exception.py
@@ -2,5 +2,18 @@ import pytest
 
 from activity.main import *
 
+def test_exception_not_raised():
+    result = having_an_exception("Some country name")
+    assert result is True
+
 def test_exception():
-    assert True
+    with pytest.raises(ValueError):
+        having_an_exception("Texas")
+
+
+# Ex.2
+# def test_empty_dict_raises_keyerror():
+#     movies = {}
+
+#     with pytest.raises(KeyError):
+#         valid_movie = find_shrek_2(movies)
